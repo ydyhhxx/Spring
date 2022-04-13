4b01f969a802fe48a3d62b8b1229968c7e71a1f2
liyanwei <lyw0425@>
2022-04-14 06:22:53 +0800

    依赖注入

--- src/main/java/com/lyw/spring/ApplicationContext.java
+++ src/main/java/com/lyw/spring/ApplicationContext.java
@@ -2,7 +2,10 @@ package com.lyw.spring;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.beans.Introspector;
 import java.io.File;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -10,24 +13,28 @@ public class ApplicationContext {
 
     private Class configClass;
 
+    // 存放 beanDefinition
     private ConcurrentHashMap<String, BeanDefinition> beanDefinitionMap = new ConcurrentHashMap(64);
+    // 单例池
+    private ConcurrentHashMap<String, Object> singletonObjects = new ConcurrentHashMap(64);
 
     public ApplicationContext(Class configClass) {
         this.configClass = configClass;
+
         // 是否配置了包扫描路径
         ComponentScan scan = (ComponentScan) configClass.getAnnotation(ComponentScan.class);
         if (scan == null)
-            throw new RuntimeException("未配置@ComponentScan");
+            throw new NullPointerException("未配置@ComponentScan");
         String path = scan.value(); // com.lyw.controller
         if (StringUtils.isBlank(path))
-            throw new RuntimeException("包扫描路径是空");
+            throw new NullPointerException("包扫描路径是空");
 
         path = path.replace(".", "/"); // com/lyw/controller
         ClassLoader classLoader = ApplicationContext.class.getClassLoader();
         // resource=file:/Users/ywli/Desktop/mytest/workspace/Spring/target/classes/com/lyw/controller
         URL resource = classLoader.getResource(path);
         if (resource == null)
-            throw new RuntimeException("路径不存在");
+            throw new NullPointerException("路径不存在");
 
         File file = new File(resource.getFile());
         if (file.isDirectory()) {
@@ -59,18 +66,67 @@ public class ApplicationContext {
                         beanDefinition.setScope(StringUtils.isBlank(value) ? "singleton" : value);
                     }
                     String beanName = component.value();
+                    if ("".equals(beanName))
+                        beanName = Introspector.decapitalize(loadClass.getSimpleName());
+
                     beanDefinition.setType(loadClass);
                     beanDefinition.setBeanName(beanName);
 
                     beanDefinitionMap.put(beanName, beanDefinition);
+
+                    String finalBeanName = beanName;
+                    beanDefinitionMap.forEach((k, v) -> {
+                        Object bean = createBean(finalBeanName, beanDefinition);
+                        singletonObjects.put(finalBeanName, bean);
+                    });
                 }
             } catch (ClassNotFoundException e) {
             }
         }
     }
 
-    public Object getBean(String name) {
+    private Object createBean(String beanName, BeanDefinition beanDefinition) {
+        Class clazz = beanDefinition.getType();
+        Object instance;
+        try {
+            instance = clazz.getConstructor().newInstance();
+
+            // 依赖注入
+            Field[] fields = clazz.getDeclaredFields();
+            for (Field f : fields) {
+                if (f.isAnnotationPresent(AutoWired.class)) {
+                    f.setAccessible(true);
+                    f.set(clazz, getBean(f.getName()));
+                }
+            }
+
+            return instance;
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
         return null;
+    }
+
+    public Object getBean(String beanName) {
+        BeanDefinition beanDefinition = beanDefinitionMap.get(beanName);
+        if (beanDefinition == null)
+            throw new NullPointerException("beanDefinition不存在");
+
+        if (!"singleton".equals(beanDefinition.getScope()))
+            return createBean(beanName, beanDefinition);
+
+        Object obj = singletonObjects.get(beanName);
+        if (obj == null) {
+            obj = createBean(beanName, beanDefinition);
+            singletonObjects.put(beanName, obj);
+        }
+        return obj;
     }
 
     public Object getBean(Class clazz) {
